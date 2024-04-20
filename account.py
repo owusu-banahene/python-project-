105bace64ef81ea79c43cffad74d3bb0dabfa150
Stephen Yeboah <stephenoyeboah@gmail.com>
2024-04-20 12:51:54 -0700

    added an account class

---
+++ account.py
@@ -0,0 +1,11 @@
+class Account:
+    def check_password_length(self, password:str):
+        if len(password) > 8:
+            return True
+        else:
+            return False
+
+
+if __name__ == '__main__':
+    account = Account()
+    print(f'The password length is {account.check_password_length("offtoschool"}')
